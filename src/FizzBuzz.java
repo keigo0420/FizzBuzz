966c4292b7a635992f4ba161bbef12dda15bcf3d
KeigoIshibashi <105145869+keigo0420@users.noreply.github.com>
2022-05-29 17:49:43 +0900

    課題訂正

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,9 +2,9 @@
 public class FizzBuzz {
 	public static void main(String[]args) {
 		for(int i=1; i<=100; i++ ) {
-			
-			if(i%15==0) {
-				System.out.println("FIzzBuzz");
+			//
+			if(i%15==0) {//‚R‚Å‚à‚T‚Å‚àŠ„‚ê‚é”
+				System.out.println("FizzBuzz");
 			}else if(i%3==0) {
 				System.out.println("Fizz");
 			}else if (i%5==0) {
